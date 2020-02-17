ead9d03086611f8840700517dcd17c31cdc8374c
jlenagala <jananianut@gmail.com>
2020-02-17 20:59:40 +0530

    Remove unneccery Folder

--- Day 4/micorservices/allocation-service/src/main/java/tpl/javatraining/allocationservice/repository/AllocationRepository.java
+++
@@ -1,7 +0,0 @@
-package tpl.javatraining.allocationservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tpl.javatraining.allocationservice.model.Allocation;
-
-public interface AllocationRepository extends JpaRepository<Allocation,Integer> {
-}
--- Day 4/micorservices/employee-services/src/main/java/tlp/javatraining/employeeservices/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package tlp.javatraining.employeeservices.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tlp.javatraining.employeeservices.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-    //Employee save(Employee employee);
-}
