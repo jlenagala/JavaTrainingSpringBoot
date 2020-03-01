45f9236dccd582f2bc5c176e4ecf3d6c6bd25079
jlenagala <jananianut@gmail.com>
2020-03-01 15:17:22 +0530

    Day 7 Connect oauth server and employee service

--- micorservices/employee-services/src/main/java/tlp/javatraining/employeeservices/EmployeeServicesApplication.java
+++ micorservices/employee-services/src/main/java/tlp/javatraining/employeeservices/EmployeeServicesApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication()
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 public class EmployeeServicesApplication {
 
 	public static void main(String[] args) {
